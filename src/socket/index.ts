875aaaf027f08956635b708c1151fd3a55543021
Sabin789 <87514484+Sabin789@users.noreply.github.com>
2023-08-01 12:45:29 +0100

    Added the option to join and leave rooms through Socket.Io

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -8,24 +8,27 @@ let newRoom: string
 let displayedMessages: any = []
 
 
-interface MySocket extends Socket {
-    data: any; 
-  }
-
+// interface MySocket extends Socket {
+//     data: any; 
+//   }
+let userId:any
 export const newConnectionHandler=(socket:Socket)=>{
   console.log(`New userJoined their id is ${socket.id}`)
   socket.emit("Welcome", socket.id)
 
   socket.on("SetUser",(data:{token:string})=>{
     const { token } = data
+  
     const secret = process.env.JWT_SECRET as string
     
     jwt.verify(token, secret, (err, decoded: any) => {
-        console.log("Verifi")
+      
         if (err) {
+            console.log("token",token)
             console.log("Token verification failed:", err);
           } else {
             socket.data = decoded
+            userId=decoded._id
             onlineUserList.push({
                 email: decoded.email,
                 _id: decoded._id,
@@ -36,35 +39,36 @@ export const newConnectionHandler=(socket:Socket)=>{
           }
     })
   })
-  socket.emit("SetUser")
   console.log(socket.data)
-  socket.on("join-room", async (room: string) => {
+  socket.on("join-room", async (data: { room: string }) => {
     
     const user = socket.data
     console.log("Data:",user)
     const newMembers:any=[]
+    console.log(data.room)
     if (!user) {
       console.log("User not found. Please authenticate first.");
       return;
     }
   
-    const chatRoom = await ChatModel.findByPk(room);
+    const chatRoom = await ChatModel.findByPk(data.room);
   
     if (!chatRoom) {
  
-      console.log(`Chat room with ID ${room} not found.`);
+      console.log(`Chat room with ID ${data.room} not found.`);
       return;
     }
 
-    newMembers.push(user)
+    newMembers.push(user._id)
+    console.log(user)
     console.log(newMembers)
     await chatRoom.update({
         members:newMembers,
       })
 
-    console.log(`User with ID ${user} joined chat room with ID ${room}`);
+    console.log(`User with ID ${user} joined chat room with ID ${data.room}`);
   
-    socket.join(room);
+    socket.join(data.room);
   });
 
 
@@ -78,16 +82,46 @@ export const newConnectionHandler=(socket:Socket)=>{
 
   })
 
-  socket.on("disconnect", () => {
-    onlineUserList = onlineUserList.filter(
-      (a: any) => a.socketId !== socket.id
-    )})
+
+socket.on("leave-room",async (data: { room: string })=>{
+    try {
+        const user = socket.data
+        console.log("Data:",user)
+        let newMembers:any=[]
+        
+        console.log(data.room)
+        if (!user) {
+          console.log("User not found. Please authenticate first.");
+          return;
+        }
+      
+        const chatRoom = await ChatModel.findByPk(data.room);
+      
+        if (!chatRoom) {
+     
+          console.log(`Chat room with ID ${data.room} not found.`);
+          return;
+        }
+
+        newMembers=chatRoom.members
+        newMembers=newMembers.filter((a: any) => a !== userId)
+        console.log("user:",user,"socket:",socket.id)
+        console.log("newmemes",chatRoom.members)
+        await chatRoom.update({
+            members:newMembers,
+          })
+
+        console.log(`User with ID ${user._id} left chat room with ID ${data.room}`);
+        socket.leave(data.room);
+    } catch (error) {
+        console.log(error)
+    }
+})
+//   socket.on("disconnect", () => {
+//     onlineUserList = onlineUserList.filter(
+//       (a: any) => a.socketId !== socket.id
+//     )})
 
 
-
-
-    // socket.broadcast.emit("updateOnlineUsersList", onlineUserList);
-    // console.log(`User with socketId of ${socket.id} disconnected`);
-    // console.log(onlineUserList);
 
 }
